8cfc5f3958c6b045eb3bc3135c54f5bd19f7524e
AlexandraViasu <viasu.alexandra@gmail.com>
2023-05-31 17:00:18 +0100

    Homework Day 3 Week 1

---
+++ src/python_functions_practice.py
@@ -0,0 +1,42 @@
+def return_10():
+    return 10
+
+def add(x, y):
+    return x + y
+
+def subtract(a, b):
+    return a - b
+
+def multiply(c, d):
+    return c * d
+
+def divide(e, f):
+    return e / f
+
+def length_of_string(string_1):
+    return len("A string of length 21")
+
+def join_string(string_3, string_4):
+    return string_3 + string_4
+
+def add_string_as_number(string_5, string_6):
+    return int(string_5) + int(string_6)
+
+def number_to_full_month_name(m):
+    if m == 1:
+        return "January"
+    if m == 3:
+        return "March"
+    if m == 9:
+        return "September"
+
+def number_to_short_month_name(n):
+    if n == 1:
+        return "Jan"
+    if n == 4:
+        return "Apr"
+    if n == 10:
+        return "Oct"
+    
+
+
