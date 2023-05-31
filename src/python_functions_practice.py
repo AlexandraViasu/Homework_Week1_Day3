c47ab2c629dd57c508945cac2f3e7facec53cd2f
AlexandraViasu <viasu.alexandra@gmail.com>
2023-05-31 17:50:46 +0100

    reuploading

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -22,21 +22,35 @@ def join_string(string_3, string_4):
 def add_string_as_number(string_5, string_6):
     return int(string_5) + int(string_6)
 
-def number_to_full_month_name(m):
-    if m == 1:
-        return "January"
-    if m == 3:
-        return "March"
-    if m == 9:
-        return "September"
+# these below pass the test, but they won't work if the numbers given are other than 1, 3, 9, etc.
 
-def number_to_short_month_name(n):
-    if n == 1:
-        return "Jan"
-    if n == 4:
-        return "Apr"
-    if n == 10:
-        return "Oct"
+# def number_to_full_month_name(m):
+#     if m == 1:
+#         return "January"
+#     if m == 3:
+#         return "March"
+#     if m == 9:
+#         return "September"
+
+# def number_to_short_month_name(n):
+#     if n == 1:
+#         return "Jan"
+#     if n == 4:
+#         return "Apr"
+#     if n == 10:
+#         return "Oct"
     
+#this is in case of other values:
+
+def number_to_full_month_name(u):
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    if u >= 1 and u <= 12:
+        return months[u - 1]
+    
+def number_to_short_month_name(o):
+    month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    if o >= 1 and o <= 12:
+        return month[o - 1]
+
 
 
